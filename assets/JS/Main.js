1f45ecc2182c95495033fc931861019e08db5c37
Yaye22 <yaye.malon.abdoul.wahab.traore@efrei.net>
2022-03-19 21:41:11 +0100

    Partie 5/8

--- assets/JS/Main.js
+++ assets/JS/Main.js
@@ -28,6 +28,28 @@
                 })
 
             }
+
+
+            window.onscroll = function() {
+
+                if (document.documentElement.scrollTop > 80) 
+                {
+
+                document.getElementById("navbar").style.background = "black";
+
+                document.getElementById("navbar").style.padding = "12px 10px";
+
+                } 
+                else 
+                {
+
+                document.getElementById("navbar").style.background = "none";
+
+                document.getElementById("navbar").style.padding = "0px 0px";
+
+                }
+
+            }
             
             
      
